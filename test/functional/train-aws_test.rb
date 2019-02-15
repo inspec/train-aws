e1568a2afeb152bfd8ef88ca092949dd36031f28
Clinton Wolfe <clintoncwolfe@gmail.com>
2019-02-15 13:17:27 -0500

    Functional test works
    Signed-off-by: Clinton Wolfe <clintoncwolfe@gmail.com>

--- test/functional/train-aws_test.rb
+++ test/functional/train-aws_test.rb
@@ -7,47 +7,24 @@
 # their app. Unlike unit tests, we don't assume any knowledge of how anything
 # works; we just know what it is supposed to do.
 
-# Include our test harness
 require_relative '../helper'
+require 'train'
 
-# Because InSpec is a Spec-style test suite, and Train has a close relationship
-# to InSpec, we're going to use MiniTest::Spec here, for familiar look and
-# feel. However, this isn't InSpec (or RSpec) code.
+# Train should auto-load train-aws
+# require 'train-aws'
+
 describe 'train-aws' do
-  # Our helper.rb locates this library from the Train install that
-  # Bundler installed for us. If we want its methods, we still must
-  # import it.  Including it here will make it available in all child
-  # 'describe' blocks.
-  include TrainPluginFunctionalHelper
-
-  # When thinking up scenarios to test, start with the simplest.
-  # Then think of each major feature, and exercise them.
-  # Running combinations of features makes sense if it is very likely,
-  # or a difficult / dangerous case.  You can always add more tests
-  # here as users find subtle problems.  In fact, having a user submit
-  # a PR that creates a failing functional test is a great way to
-  # capture the reproduction case.
-
-  # Some tests through here use minitest Expectations, which attach to all
-  # Objects, and begin with 'must' (positive) or 'wont' (negative)
-  # See https://ruby-doc.org/stdlib-2.1.0/libdoc/minitest/rdoc/MiniTest/Expectations.html
-
   describe "creating a train instance with this transport" do
-    # This is a bit of an awkward test. There is no 'wont_raise', so
-    # we just execute the risky code; if it breaks, the test will be
-    # registered as an Error.
-
     it "should not explode on create" do
       # This checks for uncaught exceptions.
-      Train.create('aws')
+      Train.create('aws', {})
 
       # This checks for warnings (or any other output) to stdout/stderr
       proc { Train.create('aws') }.must_be_silent
     end
   end
 
-  describe 'connecting using the transport'
-
+  describe 'creating a connection object using the transport' do
     it "should not explode on connect" do
       # This checks for uncaught exceptions.
       Train.create('aws').connection
@@ -60,7 +37,7 @@ describe 'train-aws' do
   describe "reading a file" do
     it "should throw an exception if you try to read a file" do
       conn = Train.create('aws').connection
-      path = File.join(plugin_fixtures_path, 'nonesuch')
+      path = File.join('nonesuch' + rand.to_s)
       proc { conn.file(path).contents }.must_raise(NotImplementedError)
     end
   end
